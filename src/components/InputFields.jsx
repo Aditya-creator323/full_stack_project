d377372c1bfbd657dc73e13c83b8a189a07eea1f
Aditya Chouhan <chouhanad8@gmail.com>
2024-08-12 20:57:49 +0530

    Step-4 integretion part

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -10,8 +10,18 @@ export default function InputFields() {
   const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
-    const defaultTableData = {id: '1', name: 'Aditya', email: 'googleemail@gmail.com'};
+    const defaultTableData = {
+      id: "1",
+      name: "Aditya",
+      email: "googleemail@gmail.com",
+    };
     setTableData([defaultTableData]);
+    fetch("http://localhost:8080/employee")
+      .then((response) => response.json())
+      .then((data) => setTableData(data))
+      .catch((error) => console.error("Error fetching data : ", error));
+
+    console.log("useeffect is called");
   }, []);
 
   const handleChange = (event) => {
@@ -22,28 +32,58 @@ export default function InputFields() {
     });
   };
 
-  const handleAdd = () => {
-    setTableData([...tableData, formData]);
+  const handleAdd = (event) => {
+    event.preventDefault();
+    const queryParams = new URLSearchParams({
+      name: formData.name,
+      email: formData.email,
+    });
 
-    setFormData({ id: "", name: "", email: "" });
+    fetch(`http://localhost:8080/addEmployee?${queryParams.toString()}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTableData([...tableData, data]);
+        setFormData({ id: "", name: "", email: "" });
+      })
+      .catch((error) => console.error("Error adding employee : ", error));
   };
 
   const handleClear = () => {
     setFormData({ id: "", name: "", email: "" });
-  }
+  };
 
-  const handleDelete = () => {
+  const handleDelete = (event) => {
+    event.preventDefault();
     const idToDelete = formData.id;
     if (idToDelete) {
-        const updatedTableData = tableData.filter(item => item.id !== idToDelete);
-        setTableData(updatedTableData);
+      fetch(`http://localhost:8080/removeEmployee/${idToDelete}`, {
+        method: "DELETE",
+      })
+        .then((reponse) => reponse.text())
+        .then((message) => {
 
-        setFormData({ ...formData, id: "" });
+          const updatedTableData = tableData.filter(
+           function (item) { 
+            return item.id.toString() !== idToDelete}
+          );
+          console.log("UpdatedTableData : ",updatedTableData);
+          setTableData(updatedTableData);
+
+          setFormData({ ...formData, id: "" });
+          console.log(message);
+        })
+        .catch(error => console.error('Error Deleting employee: ', error));
     }
-    else {
-        alert('Please enter an Id to Delete');
+    else{
+      alert('Please enter an ID to delete.');
     }
-  }
+  };
 
   return (
     <div>
@@ -67,7 +107,6 @@ export default function InputFields() {
             value={formData.name}
             placeholder="Enter the Name"
             onChange={handleChange}
-            required
           />
         </div>
         <div>
@@ -78,33 +117,32 @@ export default function InputFields() {
             value={formData.email}
             placeholder="Enter the Email"
             onChange={handleChange}
-            required
           />
         </div>
-        <div>
+        <div className="buttons">
           <button onClick={handleAdd}>Add</button>
           <button onClick={handleClear}>Clear</button>
           <button onClick={handleDelete}>Delete</button>
         </div>
       </form>
 
-      <h2>Table for Entries</h2>
+      <h2>Employee Details</h2>
       <table>
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>EMAIL</th>
-            </tr>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+          </tr>
         </thead>
         <tbody>
-            {tableData.map((item, index) => (
-                <tr key={index}>
-                    <td>{item.id}</td>
-                    <td>{item.name}</td>
-                    <td>{item.email}</td>
-                </tr>
-            ))}
+          {tableData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
